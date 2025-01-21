c563413d3798f1b0388fea636c485fc8b991840b
anderson <anderson361163@gmail.com>
2025-01-20 23:52:34 -0300

    feat: Adicionando a primeira api para contar com o back-end ;

---
+++ mobile-hackaton/types/Usuario.tsx
@@ -0,0 +1,15 @@
+export type Usuario = {
+    id: string;
+    name: string;
+    email: string;
+    password: string;
+    role: string;
+  };
+  
+  export type UsuarioForm = {
+    name: string;
+    email: string;
+    password?: string;
+    role: string;
+  };
+  
