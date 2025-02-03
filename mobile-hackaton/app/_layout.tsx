fd7cebcd2dd12a782e04da0bc24e72795f9b3a62
anderson <anderson361163@gmail.com>
2025-02-02 21:47:39 -0300

    fix: Adicionando o recurso que oculta a tela de splash quando carrega o aplicativo

--- mobile-hackaton/app/_layout.tsx
+++ mobile-hackaton/app/_layout.tsx
@@ -1,11 +1,20 @@
 import { Stack, Tabs } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+
+  useEffect(() => {
+    const hideSplash = async () => {
+      await SplashScreen.hideAsync();
+    };
+
+    hideSplash(); // Esconde a splash screen assim que o layout renderizar
+  }, []);
 
   return (
       <Stack screenOptions={{ headerShown: false }}>
