5d116f8cc1ad20b15f4ad47c985c2b9ec49be3e2
anderson <anderson361163@gmail.com>
2025-02-02 23:50:50 -0300

    feat: Adicionando o contexto para o auth

---
+++ mobile-hackaton/provider/AuthContext.tsx
@@ -0,0 +1,37 @@
+import React, { createContext, ReactNode, useContext, useState } from 'react';
+
+interface IToken{
+    token: string,
+    refreshToken: string,
+    setToken: (token: string) => void
+    setRefreshToken: (token: string) => void
+}
+
+const AuthContext = createContext<IToken | null>(null);
+
+export default function AuthProvider({children}: Readonly<{children: ReactNode}>) : JSX.Element{
+  const [token, setToken] = useState<string>("");
+  const [refreshToken, setRefreshToken] = useState<string>("");
+
+  console.log(' >>> AuthProvider.token >>>'+token);
+
+  return (
+    <AuthContext.Provider value={{token, 
+                                  setToken, 
+                                  refreshToken, 
+                                  setRefreshToken}}>
+        {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth(){
+  const contexto = useContext(AuthContext);
+
+  if(!contexto){
+    throw new Error("useAuth deve ser usado dentro de um AuthProvider");
+  }
+
+  return contexto;
+
+}
