08785fc4016d0f80b16a129288f3d2801dd4531e
anderson <anderson361163@gmail.com>
2025-02-09 19:41:20 -0300

    feat: Removendo o token quando clicar no botao sair

--- mobile-hackaton/app/logado/Sair.tsx
+++ mobile-hackaton/app/logado/Sair.tsx
@@ -1,4 +1,5 @@
 import CabecalhoPrivado from '@/components/cabecalho-privado/CabecalhoPrivado';
+import { useAuth } from '@/provider/AuthContext';
 import { useRouter } from 'expo-router';
 import React, { useEffect } from 'react';
 import { Text, View } from 'react-native';
@@ -6,9 +7,11 @@ import { Text, View } from 'react-native';
 export default function Sair(){
 
   const router = useRouter();
+  const {setToken} = useAuth();
 
   // Redirecionar automaticamente assim que o componente for montado
   useEffect(() => {
+    setToken("");
     router.push("/");  // Redirecionamento para a tela index
   }, []);
 
@@ -16,7 +19,7 @@ export default function Sair(){
     <>
       <CabecalhoPrivado></CabecalhoPrivado>
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Lista de algo</Text>
+        <Text>Aguarde redirecionamento...</Text>
       </View>
     </>
   );
